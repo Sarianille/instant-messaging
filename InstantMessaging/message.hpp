dd7a9289e974c44fc4683dbfd0df11372a5d27c8
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2023-04-30 19:06:05 +0200

    Add message header

--- InstantMessaging/message.hpp
+++ InstantMessaging/message.hpp
@@ -4,5 +4,20 @@ class message {
 public:
 	static constexpr int max_length = 1000;
 	char msg[max_length];
-	char username[20];
+
+	struct header {
+		size_t message_length;
+		char message_length_digits[5];
+		char username[20];
+
+		void set_message_length(size_t message_length) {
+			this->message_length = message_length;
+			sprintf_s(message_length_digits, "%04d", message_length);
+		}
+
+		void set_message_length(char* message_length_digits) {
+			this->message_length = atoi(message_length_digits);
+			strcpy_s(this->message_length_digits, message_length_digits);
+		}
+	};
 };
