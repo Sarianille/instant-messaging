04ddf4b4de3979dba097f5bf093175ec69808286
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2023-04-30 19:17:45 +0200

    Change header structure

--- InstantMessaging/message.hpp
+++ InstantMessaging/message.hpp
@@ -2,22 +2,19 @@
 
 class message {
 public:
+	struct {
+		unsigned int message_length;
+		char username[20];
+	} header ; 
+
 	static constexpr int max_length = 1000;
 	char msg[max_length];
 
-	struct header {
-		size_t message_length;
-		char message_length_digits[5];
-		char username[20];
+	void set_network_byte_order() {
+		header.message_length = htonl(header.message_length);
+	}
 
-		void set_message_length(size_t message_length) {
-			this->message_length = message_length;
-			sprintf_s(message_length_digits, "%04d", message_length);
-		}
-
-		void set_message_length(char* message_length_digits) {
-			this->message_length = atoi(message_length_digits);
-			strcpy_s(this->message_length_digits, message_length_digits);
-		}
-	};
+	void set_host_byte_order() {
+		header.message_length = ntohl(header.message_length);
+	}
 };
