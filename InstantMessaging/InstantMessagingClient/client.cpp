4733feeb1052c7ee780c494f057534e0ffe8ec0e
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2023-09-20 18:57:36 +0200

    Change client log in to fit UI

--- InstantMessaging/InstantMessagingClient/client.cpp
+++ InstantMessaging/InstantMessagingClient/client.cpp
@@ -1,8 +1,8 @@
 #include "client.hpp"
 #include <string.h>
 
-void client::do_connect(const boost::asio::ip::tcp::resolver::results_type& endpoints) {
-	boost::asio::async_connect(socket_, endpoints, [this](boost::system::error_code ec, boost::asio::ip::tcp::endpoint) {
+void client::do_connect() {
+	boost::asio::async_connect(socket_, endpoints_, [this](boost::system::error_code ec, boost::asio::ip::tcp::endpoint) {
 		if (!ec) {
 			receive_header();
 		}
@@ -55,54 +55,33 @@ void client::write(const message& message) {
 }
 
 void client::close() {
-	boost::asio::post(io_context_, [this]() { socket_.close(); });
+	boost::asio::post(*io_context_, [this]() { socket_.close(); });
 }
 
 bool client::is_open() {
 	return socket_.is_open();
 }
 
-std::string set_username() {
-	std::cout << "Enter username: ";
+std::optional<client> client::create_client(char host[], int port, char username[], boost::asio::io_context& io_context) {
+	message message;
+	strcpy_s(message.header.username, username);
 
-	char username[message::max_username_length];
-	std::cin.getline(username, message::max_username_length);
+	char port_str[64];
+	sprintf_s(port_str, "%d", port);
 
-	return username;
-}
+	boost::asio::ip::tcp::resolver resolver(io_context);
+	auto endpoints = resolver.resolve(host, port_str);
 
-int main(int argc, char* argv[]) {
-	try {
-		if (argc < 3)
-		{
-			std::cout << "Host or port not specified." << std::endl;
-			return 1;
-		}
-		else if (argc > 3)
-		{
-			std::cout << "Too many arguments." << std::endl;
-			return 1;
-		}
+	client client(&io_context, std::move(endpoints));
 
-		message message;
-		strcpy_s(message.header.username, set_username().c_str());
+	return client;
 
-		boost::asio::io_context io_context;
-		boost::asio::ip::tcp::resolver resolver(io_context);
-		auto endpoints = resolver.resolve(argv[1], argv[2]);
+	/*std::thread thread([&io_context]() { io_context.run(); });
 
-		client client(io_context, endpoints);
-		std::thread thread([&io_context]() { io_context.run(); });
-
-		while (std::cin.getline(message.msg, message.max_length) && client.is_open()) {
-			message.header.message_length = std::strlen(message.msg) + 1;
-			client.write(message);
-		}
-		client.close();
-		thread.join();
+	while (std::cin.getline(message.msg, message.max_length) && client.is_open()) {
+		message.header.message_length = std::strlen(message.msg) + 1;
+		client.write(message);
 	}
-	catch (std::exception& e) {
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-	return 0;
+	client.close();
+	thread.join();*/
 }
--- InstantMessaging/InstantMessagingClient/client.hpp
+++ InstantMessaging/InstantMessagingClient/client.hpp
@@ -5,14 +5,15 @@
 #include <deque>
 #include <iostream>
 #include <string>
+#include <optional>
+
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
 
 class client {
 public:
-	client(boost::asio::io_context& io_context, const boost::asio::ip::tcp::resolver::results_type& endpoints) : io_context_(io_context), socket_(io_context) {
-		do_connect(endpoints);
-	}
-
-	void do_connect(const boost::asio::ip::tcp::resolver::results_type& endpoints);
+	void do_connect();
 
 	void receive_header();
 
@@ -24,11 +25,25 @@ public:
 
 	bool is_open();
 
+	static std::optional<client> create_client(char host[], int port, char username[], boost::asio::io_context& io_context);
+
+	client(client&& other) noexcept : socket_(std::move(other.socket_)), io_context_(other.io_context_), endpoints_(std::move(other.endpoints_)), read_message_(std::move(other.read_message_)), write_messages_(std::move(other.write_messages_)) {}
+
+	client& operator=(client&& other) noexcept {
+		socket_ = std::move(other.socket_);
+		io_context_ = other.io_context_;
+		endpoints_ = std::move(other.endpoints_);
+		read_message_ = std::move(other.read_message_);
+		write_messages_ = std::move(other.write_messages_);
+		return *this;
+	}
+
 private:
 	boost::asio::ip::tcp::socket socket_;
-	boost::asio::io_context& io_context_;
+	boost::asio::io_context* io_context_;
+	boost::asio::ip::tcp::resolver::results_type endpoints_;
 	message read_message_;
 	std::deque<message> write_messages_;
 
-
+	client(boost::asio::io_context* io_context, boost::asio::ip::tcp::resolver::results_type&& endpoints) : io_context_(io_context), socket_(*io_context), endpoints_(std::move(endpoints)) {}
 };
