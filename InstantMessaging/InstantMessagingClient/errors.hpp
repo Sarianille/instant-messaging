8a84e205aa61bd050aea846fbf9dfda90e2b8da3
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2023-09-25 15:58:29 +0200

    Add error

--- InstantMessaging/InstantMessagingClient/errors.hpp
+++ InstantMessaging/InstantMessagingClient/errors.hpp
@@ -8,11 +8,13 @@ namespace errors {
 		connection_aborted = 1236,
 		server_disconnected = 10054,
 		connection_timed_out = 10060,
+		connection_refused = 10061
 	};
 
 	static constexpr char connection_aborted_error_message[] = "Connection aborted";
 	static constexpr char server_disconnected_error_message[] = "Server disconnected";
 	static constexpr char connection_timed_out_error_message[] = "Connection timed out";
+	static constexpr char connection_refused_error_message[] = "Connection refused (is this server running?)";
 
 	std::optional<const char*> get_error_message(const boost::system::error_code& ec) {
 		switch (ec.value()) {
@@ -22,6 +24,8 @@ namespace errors {
 			return server_disconnected_error_message;
 		case connection_timed_out:
 			return connection_timed_out_error_message;
+		case connection_refused:
+			return connection_refused_error_message;
 		default:
 			return std::nullopt;
 		}
