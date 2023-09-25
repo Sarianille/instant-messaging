2e12ccdb76d878511c7555a82870662b4d4ff92c
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2023-09-25 15:54:08 +0200

    Refactor client

--- InstantMessaging/InstantMessagingClient/client.cpp
+++ InstantMessaging/InstantMessagingClient/client.cpp
@@ -2,6 +2,62 @@
 #include "errors.hpp"
 #include <string.h>
 
+void message_handler::render_messages() {
+	std::lock_guard<std::mutex> lock(read_messages_mutex_);
+	int read_messages_new_size = read_messages_.size();
+
+	if (read_messages_new_size > read_messages_old_size_) {
+		ImGui::SetScrollHereY(-1.0f);
+	}
+
+	while (read_messages_new_size > max_messages) {
+		read_messages_.pop_front();
+		read_messages_new_size--;
+	}
+
+	read_messages_old_size_ = read_messages_new_size;
+
+	for (auto& message : read_messages_) {
+		ImGui::TextWrapped(message.c_str());
+	}
+}
+
+void message_handler::save_incoming_message(const message& message) {
+	std::stringstream new_message = std::stringstream();
+	new_message << "[" << message.header.username << "]: " << message.msg;
+
+	std::lock_guard<std::mutex> lock(read_messages_mutex_);
+	read_messages_.push_back(std::move(new_message.str()));
+}
+
+void message_handler::save_incoming_message(message&& message) {
+	std::stringstream new_message = std::stringstream();
+	new_message << "[" << message.header.username << "]: " << message.msg;
+
+	std::lock_guard<std::mutex> lock(read_messages_mutex_);
+	read_messages_.push_back(std::move(new_message.str()));
+}
+
+bool message_handler::write_queue_empty() {
+	return write_messages_.empty();
+}
+
+void message_handler::enqueue_message_to_be_written(const message& message) {
+	write_messages_.push_back(message);
+}
+
+void message_handler::enqueue_message_to_be_written(message&& message) {
+	write_messages_.push_back(std::move(message));
+}
+
+message& message_handler::message_to_be_written() {
+	return write_messages_.front();
+}
+
+void message_handler::pop_message_to_be_written() {
+	write_messages_.pop_front();
+}
+
 void client::do_connect() {
 	boost::asio::async_connect(socket_, endpoints_, [this](boost::system::error_code ec, boost::asio::ip::tcp::endpoint) {
 		if (!ec) {
@@ -14,9 +70,9 @@ void client::do_connect() {
 }
 
 void client::receive_header() {
-	boost::asio::async_read(socket_, boost::asio::buffer(&read_message_, sizeof(message::header)), [this](boost::system::error_code ec, size_t read_bytes) {
+	boost::asio::async_read(socket_, boost::asio::buffer(&message_handler_.read_message_, sizeof(message::header)), [this](boost::system::error_code ec, size_t read_bytes) {
 		if (!ec) {
-			read_message_.set_host_byte_order();
+			message_handler_.read_message_.set_host_byte_order();
 			receive_body();
 		}
 		else {
@@ -25,9 +81,9 @@ void client::receive_header() {
 }
 
 void client::receive_body() {
-	boost::asio::async_read(socket_, boost::asio::buffer(read_message_.msg, read_message_.header.message_length), [this](boost::system::error_code ec, size_t read_bytes) {
+	boost::asio::async_read(socket_, boost::asio::buffer(message_handler_.read_message_.msg, message_handler_.read_message_.header.message_length), [this](boost::system::error_code ec, size_t read_bytes) {
 		if (!ec) {
-			save_new_message(std::move(read_message_));
+			save_new_message(std::move(message_handler_.read_message_));
 			receive_header();
 		}
 		else {
@@ -41,9 +97,10 @@ void client::write(char content[]) {
 	message.header.message_length = strlen(content) + 1;
 	strcpy_s(message.msg, content);
 
-	bool write_in_progress = !write_messages_.empty();
-	write_messages_.push_back(message);
+	bool write_in_progress = !message_handler_.write_queue_empty();
+	message_handler_.enqueue_message_to_be_written(message);
 	save_new_message(std::move(message));
+
 	if (!write_in_progress) {
 		write_new_message();
 	}
@@ -53,38 +110,22 @@ void client::close() {
 	boost::asio::post(*io_context_, [this]() {
 		if (is_open())
 		{
-			//socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
 			socket_.close();
 		}
 		io_context_->stop();
 		});
-
-	/*socket_.close();
-	io_context_->stop();*/
 }
 
 bool client::is_open() {
 	return is_connected_;
 }
 
+void client::clear_error_message() {
+	error_message_.reset();
+}
+
 void client::render_messages() {
-	std::lock_guard<std::mutex> lock(read_messages_mutex_);
-	int read_messages_new_size = read_messages_.size();
-
-	if (read_messages_new_size > read_messages_old_size_) {
-		ImGui::SetScrollHereY(1.0f);
-	}
-
-	while (read_messages_new_size > max_messages) {
-		read_messages_.pop_front();
-		read_messages_new_size--;
-	}
-
-	read_messages_old_size_ = read_messages_new_size;
-	
-	for (auto& message : read_messages_) {
-		ImGui::TextWrapped(message.c_str());
-	}
+	message_handler_.render_messages();
 }
 
 client client::create_client(char host[], int port, char username[], boost::asio::io_context& io_context) {
@@ -103,26 +144,18 @@ std::optional<std::string>& client::get_error_message() {
 	return error_message_;
 }
 
-void client::clear_error_message() {
-	error_message_.reset();
-}
-
 void client::save_new_message(message&& message) {
-	std::stringstream new_message = std::stringstream();
-	new_message << "[" << message.header.username << "]: " << message.msg;
-
-	std::lock_guard<std::mutex> lock(read_messages_mutex_);
-	read_messages_.push_back(std::move(new_message.str()));
+	message_handler_.save_incoming_message(std::move(message));
 }
 
 void client::write_new_message() {
-	if (!write_messages_.empty()) {
-		unsigned int message_length = write_messages_.front().header.message_length;
-		write_messages_.front().set_network_byte_order();
+	if (!message_handler_.write_queue_empty()) {
+		unsigned int message_length = message_handler_.message_to_be_written().header.message_length;
+		message_handler_.message_to_be_written().set_network_byte_order();
 
-		boost::asio::async_write(socket_, boost::asio::buffer(&write_messages_.front(), sizeof(message::header) + message_length), [this](boost::system::error_code ec, size_t written_bytes) {
+		boost::asio::async_write(socket_, boost::asio::buffer(&message_handler_.message_to_be_written(), sizeof(message::header) + message_length), [this](boost::system::error_code ec, size_t written_bytes) {
 			if (!ec) {
-				write_messages_.pop_front();
+				message_handler_.pop_message_to_be_written();
 				write_new_message();
 			}
 			else {
--- InstantMessaging/InstantMessagingClient/client.hpp
+++ InstantMessaging/InstantMessagingClient/client.hpp
@@ -16,6 +16,52 @@
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx11.h"
 
+class message_handler {
+public:
+	message read_message_;
+
+	void render_messages();
+
+	void save_incoming_message(const message& message);
+
+	void save_incoming_message(message&& message);
+
+	bool write_queue_empty();
+
+	void enqueue_message_to_be_written(const message& message);
+
+	void enqueue_message_to_be_written(message&& message);
+
+	message& message_to_be_written();
+
+	void pop_message_to_be_written();
+
+	message_handler(message_handler&& other) noexcept : read_message_(std::move(other.read_message_)), read_messages_mutex_(),
+		read_messages_old_size_(other.read_messages_old_size_), read_messages_(std::move(other.read_messages_)), write_messages_(std::move(other.write_messages_)) { }
+
+	message_handler& operator=(message_handler&& other) noexcept {
+		read_message_ = std::move(other.read_message_);
+		read_messages_old_size_ = other.read_messages_old_size_;
+		read_messages_ = std::move(other.read_messages_);
+		write_messages_ = std::move(other.write_messages_);
+		return *this;
+	}
+
+	message_handler() : read_message_(), read_messages_mutex_(), read_messages_old_size_(0), read_messages_(), write_messages_() { }
+
+private:
+	static constexpr int max_messages = 100;
+
+	std::mutex read_messages_mutex_;
+	std::size_t read_messages_old_size_;
+	std::deque<std::string> read_messages_;
+	std::deque<message> write_messages_;
+
+	message_handler(const message_handler&) = delete;
+
+	message_handler& operator=(const message_handler&) = delete;
+};
+
 class client {
 public:
 	void do_connect();
@@ -30,43 +76,35 @@ public:
 
 	bool is_open();
 
+	void clear_error_message();
+
 	void render_messages();
 
 	static client create_client(char host[], int port, char username[], boost::asio::io_context& io_context);
 
 	std::optional<std::string>& get_error_message();
 
-	void clear_error_message();
-
-	client(client&& other) noexcept : socket_(std::move(other.socket_)), io_context_(other.io_context_), endpoints_(std::move(other.endpoints_)), read_message_(std::move(other.read_message_)), read_messages_(std::move(other.read_messages_)), write_messages_(std::move(other.write_messages_)), read_messages_old_size_(other.read_messages_old_size_), error_message_(other.error_message_), read_messages_mutex_(), is_connected_(other.is_connected_.load()) {
+	client(client&& other) noexcept : username_(), message_handler_(std::move(other.message_handler_)), socket_(std::move(other.socket_)), io_context_(other.io_context_), endpoints_(std::move(other.endpoints_)), error_message_(std::move(other.error_message_)), is_connected_(other.is_connected_.load()) {
 		strcpy_s(username_, other.username_);
 	}
 
 	client& operator=(client&& other) noexcept {
+		strcpy_s(username_, other.username_);
+		message_handler_ = std::move(other.message_handler_);
 		socket_ = std::move(other.socket_);
 		io_context_ = other.io_context_;
 		endpoints_ = std::move(other.endpoints_);
-		read_message_ = std::move(other.read_message_);
-		read_messages_ = std::move(other.read_messages_);
-		write_messages_ = std::move(other.write_messages_);
-		strcpy_s(username_, other.username_);
-		read_messages_old_size_ = other.read_messages_old_size_;
-		error_message_ = other.error_message_;
-		is_connected_.store(other.is_connected_.load());
+		error_message_ = std::move(other.error_message_);
+		is_connected_ = other.is_connected_.load();
 		return *this;
 	}
 
 private:
+	char username_[20];
+	message_handler message_handler_;
 	boost::asio::ip::tcp::socket socket_;
 	boost::asio::io_context* io_context_;
 	boost::asio::ip::tcp::resolver::results_type endpoints_;
-	char username_[20];
-	message read_message_;
-	std::deque<std::string> read_messages_;
-	std::deque<message> write_messages_;
-	std::mutex read_messages_mutex_;
-	int read_messages_old_size_;
-	static constexpr int max_messages = 100;
 	std::optional<std::string> error_message_ = std::nullopt;
 	std::atomic<bool> is_connected_ = false;
 
